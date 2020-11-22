1dda8676c6e67468ef03c1ffacc32a44c77d142f
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-22 18:31:35 +0900

    [CREATE] <com.dsm.xlsxparser.NightSelfLearningSupervisionTableGeneratorApplication> Main Class(Hello, world!)

---
+++ src/main/java/com/dsm/xlsxparser/NightSelfLearningSupervisionTableGeneratorApplication.java
@@ -0,0 +1,7 @@
+package com.dsm.xlsxparser;
+
+public class NightSelfLearningSupervisionTableGeneratorApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
